0017bcaef3c34c8f3c0e3b4e5af6f5218f076254
admin <admin@BTSWIN7-99>
2019-01-30 16:41:51 +0100

    Classe métier LigneDeFrais

---
+++ Labo GSB/Metier/metierFrais/LigneDeFrais.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_GSB.Metier.metierFrais
+{
+    class LigneDeFrais
+    {
+        public int Id { get; }
+        public DateTime Date { get; protected set; }
+        public int Montant { get; protected set; }
+        public string Libelle { get; protected set; }
+        public bool Validee { get; protected set; }
+        public bool Refusee { get; protected set; }
+        public bool HorsForfait { get; protected set; }
+        public bool Report { get; protected set; }
+
+        public LigneDeFrais(
+            int id,
+            DateTime date,
+            int montant,
+            string libelle,
+            bool validee,
+            bool refusee,
+            bool horsForfait,
+            bool report
+            )
+        {
+            this.Id = id;
+            this.Date = date;
+            this.Montant = montant;
+            this.Libelle = libelle;
+            this.Validee = validee;
+            this.Refusee = refusee;
+            this.HorsForfait = horsForfait;
+            this.Report = report;
+        }
+
+    }
+
+}
