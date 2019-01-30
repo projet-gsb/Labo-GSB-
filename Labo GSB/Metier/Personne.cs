5b949f18a2877c3583b727d0b684f6cca5ddcdfa
bourgeois gaetan <gaet.bourgeois@gmail.com>
2019-01-30 12:20:20 +0100

    premier push

---
+++ Labo GSB/Metier/Personne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_GSB.Metier.personne
+{
+    public interface IPersonne
+    {
+    }
+
+    public abstract class Personne : IPersonne
+    {
+    }
+
+    class VisiteurMedical : Personne
+    {
+    }
+
+    class Contact : Personne
+    {
+    }
+}
