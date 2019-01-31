81e884c98cd1bc32c7829ad8ae2274fca04f7b47
clem56 <clement.savary@yahoo.fr>
2019-01-31 15:50:04 +0100

    debut crud compte rendu

---
+++ Labo GSB/DAO/daoCR/CompteRenduDAO.cs
@@ -0,0 +1,50 @@
+﻿using Labo_GSB.Metier.metierCR;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_GSB.DAO.daoCR
+{
+    class CompteRenduDAO : DAO<CompteRendu>
+    {
+        public override void Create(CompteRendu objet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(CompteRendu objet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Read(int id)
+        {
+            SqlCommand commande = Connexion.GetInstance().CreateCommand();
+            commande.CommandText = "SELECT * FROM compterendu WHERE id = @id";
+            commande.Parameters.AddWithValue("@id", id);
+            SqlDataReader datareader = commande.ExecuteReader();
+            while(datareader.Read())
+            {
+                int Id = id;
+                int IdVisiteurMedical = idVisiteurMedical;
+                int IdContact = idContact;
+                int IdEtablissement = idEtablissement;
+
+
+
+
+
+                string nom = dataReader.GetString(1);
+
+            }
+        }
+
+        public override void Update(CompteRendu objet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
