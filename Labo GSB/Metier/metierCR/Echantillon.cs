de0a86e012e27bf2d50d7acb5a65f180b8812ca1
clem56 <clement.savary@yahoo.fr>
2019-02-01 17:25:52 +0100

    ajout classe metier et crud echantillon

---
+++ Labo GSB/Metier/metierCR/Echantillon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_GSB.Metier.metierCR
+{
+    class Echantillon
+    {
+        private int _idCompteRendu;
+        private int _idProduit;
+        private int _quantite;
+
+        public int IdCompteRendu { get => _idCompteRendu; protected set => _idCompteRendu = value; }
+        public int IdProduit { get => _idProduit; protected set => _idProduit = value; }
+        public int Quantite { get => _quantite; protected set => _quantite = value; }
+
+        public Echantillon(int idCompteRendu, int idProduit,int quantite)
+        {
+            this.IdCompteRendu = idCompteRendu;
+            this.IdProduit = idProduit;
+            this.Quantite = quantite;
+        }
+    }
+}
