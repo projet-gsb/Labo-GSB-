dc001849b7f906b5029b074d400e5d8188f5d410
admin <admin@BTSWIN7-99>
2019-02-05 22:45:01 +0100

    Metier FicheDeFrais

---
+++ Labo GSB/Metier/metierFrais/FicheDeFrais.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_GSB.Metier.metierFrais{
+    
+    //BDD
+    class FicheDeFrais{
+
+        public int Id { get; }
+        public int IdVisiteur { get; }
+        public DateTime DateCreation { get; protected set; }
+        public DateTime DateTraitement { get; protected set; }
+        public bool MiseEnPaiement { get; protected set; }
+
+        public FicheDeFrais (int id, int idvisiteur, DateTime datecreation, DateTime datetraitement, bool miseenpaiement){
+            this.Id = id;
+            this.IdVisiteur = idvisiteur;
+            this.DateCreation = datecreation;
+            this.DateTraitement = datetraitement;
+            this.MiseEnPaiement = miseenpaiement;
+        }
+    }
+}
