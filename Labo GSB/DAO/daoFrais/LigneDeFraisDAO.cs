d1462b302d27ce5376c12aaa9c0de36b3ee0767a
FlorentG <admin@BTSWIN7-99>
2019-02-10 11:25:36 +0100

    LigneDeFraisDAO vérifier la gestion booleen CRUD C#

--- Labo GSB/DAO/daoFrais/LigneDeFraisDAO.cs
+++ Labo GSB/DAO/daoFrais/LigneDeFraisDAO.cs
@@ -10,7 +10,7 @@ namespace Labo_GSB.DAO.daoFrais
 {
     class LigneDeFraisDAO : DAO<LigneDeFrais>
     {
-        public override void create(LigneDeFrais lignedefrais)
+        public override void Create(LigneDeFrais lignedefrais)
         {
             SqlCommand command = Connexion.GetInstance().CreateCommand();
             // Définition de la requête
@@ -26,8 +26,60 @@ namespace Labo_GSB.DAO.daoFrais
             // Exécution de la requête
             // command.ExecuteNonQuery();
             // pour récupérer la clé générée
-            Int32 newId = (Int32)command.ExecuteScalar();
-            lignedefrais.setId(newId);
+            // Int32 newId = (Int32)command.ExecuteScalar();
+            int newId = (int)command.ExecuteScalar();
+            lignedefrais.Id = newId;
+        }
+
+        public override void Delete(LigneDeFrais lignedefrais)
+        {
+            SqlCommand commande = Connexion.GetInstance().CreateCommand();
+            int id = lignedefrais.Id;
+            commande.CommandText = "DELETE * FROM lignedefrais WHERE id = @id";
+            commande.Parameters.AddWithValue("@id", id);
+            commande.ExecuteNonQuery();
+
+        }
+
+        // TODO : vérifier le GetBytes(boolean) pour le booleen
+
+        public override LigneDeFrais Read(int id)
+        {
+            LigneDeFrais lignedefrais = null;
+            SqlCommand commande = Connexion.GetInstance().CreateCommand();
+            commande.CommandText = "SELECT * FROM lignedefrais WHERE id = @id";
+            commande.Parameters.AddWithValue("@id", id);
+            SqlDataReader datareader = commande.ExecuteReader();
+            while (datareader.Read())
+            {
+                int Id = id;
+                DateTime date = datareader.GetDateTime(1);
+                double montant = datareader.GetInt32(2);
+                string libelle = datareader.GetString(3);
+                bool validee = datareader.GetBytes(4);
+                bool refusee = datareader.GetBytes(5);
+                bool horsForfait = datareader.GetBytes(6);
+                bool report = datareader.GetBytes(7);
+                lignedefrais = new LigneDeFrais(id, date, montant, libelle, validee, refusee, horsForfait, report);
+            }
+            datareader.Close();
+
+            return lignedefrais;
+        }
+
+        public override void Update(LigneDeFrais lignedefrais)
+        {
+            SqlCommand commande = Connexion.GetInstance().CreateCommand();
+            commande.CommandText = "UPDATE lignedefrais  SET id = @id, date = @date, montant = @montant,libelle = @libelle, validee = @validee, refusee = @refusee, horsForfait = @horsForfait, report = @report WHERE id = @id";
+            commande.Parameters.AddWithValue("@date", lignedefrais.Date);
+            commande.Parameters.AddWithValue("@montant", lignedefrais.Montant);
+            commande.Parameters.AddWithValue("@libelle", lignedefrais.Libelle);
+            commande.Parameters.AddWithValue("@validee", lignedefrais.Validee);
+            commande.Parameters.AddWithValue("@refusee", lignedefrais.Refusee);
+            commande.Parameters.AddWithValue("@horsForfait", lignedefrais.HorsForfait);
+            commande.Parameters.AddWithValue("@report", lignedefrais.Report);
+            commande.ExecuteNonQuery();
         }
     }
+
 }
--- Labo GSB/Metier/metierFrais/LigneDeFrais.cs
+++ Labo GSB/Metier/metierFrais/LigneDeFrais.cs
@@ -9,25 +9,37 @@ namespace Labo_GSB.Metier.metierFrais
     // BDD
     class LigneDeFrais
     {
-        public int Id { get; }
+        //Vieille méthode
+        /*public int Id { get; }
         public DateTime Date { get; protected set; }
-        public int Montant { get; protected set; }
+        public double Montant { get; protected set; }
         public string Libelle { get; protected set; }
         public bool Validee { get; protected set; }
         public bool Refusee { get; protected set; }
         public bool HorsForfait { get; protected set; }
         public bool Report { get; protected set; }
+        */
 
-        public LigneDeFrais(
-            int id,
-            DateTime date,
-            int montant,
-            string libelle,
-            bool validee,
-            bool refusee,
-            bool horsForfait,
-            bool report
-            )
+        private int _id;
+        private DateTime _date;
+        private double _montant;
+        private string _libelle;
+        private bool _validee;
+        private bool _refusee;
+        private bool _horsForfait;
+        private bool _report;
+
+        public int Id { get => _id; set => _id = value; }
+        public DateTime Date { get => _date; protected set => _date = value; }
+        public double Montant { get => _montant; protected set => _montant = value; }
+        public string Libelle { get => _libelle; protected set => _libelle = value; }
+        public bool Validee { get => _validee; protected set => _validee = value; }
+        public bool Refusee { get => _refusee; protected set => _refusee = value; }
+        public bool HorsForfait { get => _horsForfait; protected set => _horsForfait = value; }
+        public bool Report { get => _report; protected set => _report = value; }
+
+
+        public LigneDeFrais(int id, DateTime date,  double montant, string libelle, bool validee, bool refusee, bool horsForfait, bool report)
         {
             this.Id = id;
             this.Date = date;
